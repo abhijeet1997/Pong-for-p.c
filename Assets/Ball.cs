af6f8a55dc95e2f5f6734b0e9b6bdb134fe54a9a
jagan1997 <jagan_mohan@srmuniv.edu.in>
2016-06-26 12:37:55 +0530

    first commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 30;
+	public static int Score = 0;
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos,
+	                float racketHeight) {
+		// ascii art:
+		// ||  1 <- at the top of the racket
+		// ||
+		// ||  0 <- at the middle of the racket
+		// ||
+		// || -1 <- at the bottom of the racket
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
+	\
+	void OnCollisionEnter2D(Collision2D col) {
+	
+		if (col.gameObject.name == "PlayerLeft") {
+			// Calculate hit Factor
+			float y = hitFactor (transform.position,
+			                    col.transform.position,
+			                    col.collider.bounds.size.y);
+			
+			// Calculate direction, make length=1 via .normalized
+			Vector2 dir = new Vector2 (1, y).normalized;
+			
+			// Set Velocity with dir * speed
+			GetComponent<Rigidbody2D> ().velocity = dir * speed;
+		}
+		
+		// Hit the right Racket?
+		if (col.gameObject.name == "PlayerRight") {
+			// Calculate hit Factor
+			float y = hitFactor (transform.position,
+			                    col.transform.position,
+			                    col.collider.bounds.size.y);
+			
+			// Calculate direction, make length=1 via .normalized
+			Vector2 dir = new Vector2 (-1, y).normalized;
+			
+			// Set Velocity with dir * speed
+			GetComponent<Rigidbody2D> ().velocity = dir * speed;
+		}
+	}	
+}
